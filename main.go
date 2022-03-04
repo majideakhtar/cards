eb253b1659302e6657a5c071895370ccbf84a618
Majid Equbal Akhtar <makhtar@msystechnologies.com>
2022-03-04 12:13:47 +0530

    15. Variable Declarations corrections
    Signed-off-by: Majid Equbal Akhtar <makhtar@msystechnologies.com>

--- main.go
+++ main.go
@@ -1,23 +1,13 @@
-// package main
-
-// import "fmt"
-
-// func main() {
-// 	// var card string = "Ace of Spades"
-// 	// var card = "Ace of Spades"
-// 	card := "Ace of Spades"
-// 	fmt.Println(card)
-
-// 	card = "Five of Diamonds"
-// 	fmt.Println(card)
-// }
 package main
 
 import "fmt"
 
-var deckSize int
-
 func main() {
-	deckSize = 50
-	fmt.Println(deckSize)
+	// var card string = "Ace of Spades"
+	// var card = "Ace of Spades"
+	card := "Ace of Spades"
+	fmt.Println(card)
+
+	card = "Five of Diamonds"
+	fmt.Println(card)
 }
