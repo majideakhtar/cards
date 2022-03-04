47cc2054bd476487559b9b0ab899a2725326b426
Majid Equbal Akhtar <makhtar@msystechnologies.com>
2022-03-04 18:48:11 +0530

    Added pgm for named return values
    Signed-off-by: Majid Equbal Akhtar <makhtar@msystechnologies.com>

---
+++ 2_named_return_values.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+)
+
+func rectProps(length, width float64) (area, perimeter float64) {
+	area = length * width
+	perimeter = (length + width) * 2
+	return //no explicit return value
+}
+
+func main() {
+	area, perimeter := rectProps(130.7, 1.5)
+	fmt.Printf("Area %f \nPerimeter %f\n", area, perimeter)
+}
