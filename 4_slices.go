aaa6e92c11cdac22a03c910ba568269efc94cff6
Majid Equbal Akhtar <makhtar@msystechnologies.com>
2022-03-07 17:03:15 +0530

    Added slice.go file
    Signed-off-by: Majid Equbal Akhtar <makhtar@msystechnologies.com>

---
+++ 4_slices.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+	cards := []string{"Ace of Spades", "Five of Diamonds"}
+	// fmt.Println(cards)
+
+	cards = append(cards, "Six of Hearts")
+	fmt.Println(cards)
+
+	// for i, card := range cards {
+	// 	fmt.Printf("\nIndex is %v, Value is %v\n", i, card)
+	// }
+
+	for _, card := range cards {
+		fmt.Printf(" Value is %v\n", card)
+	}
+}
