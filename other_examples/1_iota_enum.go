972282c6b5547163b6092399d2f2a75efb1a8808
Majid Equbal Akhtar <makhtar@msystechnologies.com>
2022-03-08 12:46:37 +0530

    added example pgm for iota enum
    Signed-off-by: Majid Equbal Akhtar <makhtar@msystechnologies.com>

---
+++ other_examples/1_iota_enum.go
@@ -0,0 +1,45 @@
+package main
+
+import "fmt"
+
+type Size uint8
+
+const (
+	small = Size(iota)
+	medium
+	large
+	extraLarge
+)
+
+func main() {
+	var m Size = 3
+	m.toString()
+}
+func (s Size) toString() {
+	switch s {
+	case small:
+		fmt.Println("Small")
+	case medium:
+		fmt.Println("Medium")
+	case large:
+		fmt.Println("Large")
+	case extraLarge:
+		fmt.Println("Extra Large")
+	default:
+		fmt.Println("Invalid Size entry")
+	}
+}
+
+// const (
+// 	small Size = iota
+// 	medium
+// 	large
+// 	extraLarge
+// )
+
+// func main() {
+// 	fmt.Println(small)
+// 	fmt.Println(medium)
+// 	fmt.Println(large)
+// 	fmt.Println(extraLarge)
+// }
